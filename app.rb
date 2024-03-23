e87f329db20cf0e1663d8da21a3c94eb7796ab2f
Muzammil Juda <150502425+Muzammil-Juda@users.noreply.github.com>
2024-03-23 04:19:12 +0000

    Created rock erb template

--- app.rb
+++ app.rb
@@ -1,8 +1,34 @@
 require 'sinatra'
+require "sinatra/reloader"
 
 get('/') do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get('/rock') do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:rock)
+end
+
+
+get('/paper') do
+
+end
+
+
+get('/scissors') do
+
 end
